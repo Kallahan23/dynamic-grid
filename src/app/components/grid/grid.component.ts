a6e617840e1bbcfee9b3c782b155a938375157e6
Kallahan23 <dankalnian23@gmail.com>
2020-05-03 20:21:21 +1000

    Fix padding inconsistencies

--- src/app/components/grid/grid.component.ts
+++ src/app/components/grid/grid.component.ts
@@ -16,7 +16,7 @@ export class GridComponent implements OnInit {
     /** Selected hour interval for table columns to display */
     selectedInterval: number;
     /** Available hour intervals to choose from */
-    intervals = [ 1, 6, 4, 24 ];
+    intervals = [ 6, 4, 24 ];
     /** Array of all hours in day: [ '00:00', '01:00', '02:00' ... '23:00' ] */
     availableTimes = Array(24).fill(0).map((x, i) => `${i}:00`.padStart(5, '0'));
 
@@ -41,14 +41,14 @@ export class GridComponent implements OnInit {
      */
     addItem(): void {
         const newItem = {
-            name: 'Item',
-            units: 'L',
+            name: '',
+            units: '',
             values: [],
         };
         newItem.values = this.availableTimes.map(time => {
             return {
                 time,
-                value: time
+                value: ''
             };
         });
         this.data.push(newItem);
